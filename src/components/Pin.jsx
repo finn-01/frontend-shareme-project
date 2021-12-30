58a85a1a840f44ef871508c13ea0dcc0b4ef7a2b
finn-01 <nguyensam576@gmail.com>
2021-12-30 23:12:21 +0700

    fix googleId

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -15,7 +15,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 
 	const user = fetchUser();
 	const alreadySaved = !!save?.filter(
-		(item) => item.postedBy._id === user.googleId
+		(item) => item.postedBy._id === user?.googleId
 	)?.length;
 	//1, [2, 3, 1] -> [1].length = 1 -> !1 -> false -> !false -> true
 	//4, [2, 3, 1] -> [].length = 0 -> !0 -> true -> !true -> false
@@ -28,10 +28,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 				.insert("after", "save[-1]", [
 					{
 						_key: uuidv4(),
-						userId: user.googleId,
+						userId: user?.googleId,
 						postedBy: {
 							_type: "postedBy",
-							_ref: user.googleId,
+							_ref: user?.googleId,
 						},
 					},
 				])
@@ -116,7 +116,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 								</a>
 							)}
 
-							{postedBy?._id === user.googleId && (
+							{postedBy?._id === user?.googleId && (
 								<button
 									type="button"
 									onClick={(e) => {
